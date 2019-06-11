0f7a98c01bc3e2aa8f1c579eb4abd5593a3466f2
Elliot Nunn <elliotnunn@me.com>
2019-06-11 16:39:49 +0800

    Make README work on PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, Extension
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup_args = dict(
     name='tbxi',
-    version='0.6',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.7',
     author='Elliot Nunn',
     author_email='elliotnunn@fastmail.com',
     description='Tools to compile and inspect Macintosh ROM images',
